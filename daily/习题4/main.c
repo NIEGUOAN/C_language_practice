28793633f13757cd590d4f5177fa8bae41422bb5
NIEGUOAN <48119707+NIEGUOAN@users.noreply.github.com>
2019-03-23 23:19:14 +0800

    2019/3/23

---
+++ daily/习题4/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("***************************\n\n");
+    printf("       Hello  world!\n\n");
+    printf("***************************\n");
+    return 0;
+}
---
+++ daily/习题6/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c,max;
+    printf("please input a,b,c: \n");
+    scanf("%d,%d,%d",&a,&b,&c);
+    max=a;
+    if(max<b)
+        max=b;
+    if(max<c)
+        max=c;
+    printf("The largest number is %d\n",max);
+    return 0;
+}
