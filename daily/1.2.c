8d928161550d91c2d1d23dd6be8858b09142d059
NIEGUOAN <48119707+NIEGUOAN@users.noreply.github.com>
2019-04-03 23:02:22 +0800

    2019/4/3(练习)

---
+++ daily/1.2.c
@@ -0,0 +1,8 @@
+﻿#pragma warning(disable:4996)
+#include<stdio.h>
+int main()
+{
+	printf("Concrete contains gravel and cement.\n");
+
+	return 0;
+}
---
+++ daily/2.3.c
@@ -0,0 +1,16 @@
+﻿#pragma warning(disable:4996)
+/*two_func.c -- 一个文件中包含两个函数*/
+#include<stdio.h>
+void butler(void);/* ANSI/ISO C 函数原型*/
+int main()
+{
+	printf("I will summon the butler function.\n");
+	butler();
+	printf("Yes.Bring me some tea and writeable DVDs.\n");
+
+	return 0;
+}
+void butler(void)/* 函数定义开始 */
+{
+	printf("You rang,sir?\n");
+}
