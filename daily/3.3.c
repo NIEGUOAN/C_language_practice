b5ea5037c04a929761340e6800028bb27ab119bd
NIEGUOAN <48119707+NIEGUOAN@users.noreply.github.com>
2019-04-05 01:27:10 +0800

    2019/4/4

---
+++ daily/3.3.c
@@ -0,0 +1,8 @@
+﻿#pragma warning(disable:4996)
+
+#include<stdio.h>
+int main()
+{
+	
+	return 0;
+}
---
+++ daily/3.5.c
@@ -0,0 +1,13 @@
+﻿#pragma warning(disable:4996)
+/* charcod.c-显示字符的代码符号 */
+#include<stdio.h>
+int main()
+{
+	char ch;
+
+	printf("Please enter a character.\n");
+	scanf("%c", &ch);/*用户输入字符*/
+	printf("The code for %c is %d.\n", ch, ch);
+
+	return 0;
+}
