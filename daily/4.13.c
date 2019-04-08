50ae436c1e193963d2dc62f5290768a50c62d058
NIEGUOAN <48119707+NIEGUOAN@users.noreply.github.com>
2019-04-08 20:07:00 +0800

    2019/4/8

---
+++ daily/4.13.c
@@ -0,0 +1,12 @@
+﻿#pragma warning(disable:4996)
+/* prntval.c -- printf() 的返回值 */
+#include<stdio.h> 
+int main()
+{
+	int bph2o = 212;
+	int rv;
+
+	rv = printf("%d F is water's boiling point.\n", bph2o);
+	printf("The printf() function printed %d character.\n", rv);
+	return 0;
+}
