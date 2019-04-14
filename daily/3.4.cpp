f7cea0dbf82427bcc0a9cd348326f662f5bbd245
NIEGUOAN <48119707+NIEGUOAN@users.noreply.github.com>
2019-04-14 18:23:32 +0800

    2019/4/14

---
+++ daily/3.4.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+using namespace std;//编译指令
+
+int main()
+{
+	int chest = 42;
+	int waist = 42;
+	int inseam = 42;
+
+	cout << "Monsieur cuts a striking figure!" << endl;
+	cout << "chest = " << chest << " (decimal for 42)" << endl;
+	cout << hex;//以后的输出都为十六进制
+	cout << "waist = " << waist << " (hexadecimal for 42)" << endl;
+	cout << oct;//以后的输出都为八进制
+	cout << "inseam = " << inseam << " (octal for 42)" << endl;
+	return 0;
+}
---
+++ daily/3.9.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+using namespace std;//编译指令
+
+int main()
+{
+	float a = 2.34e+22f;
+	float b = a + 1.0f;
+
+	cout << "a = " << a << endl;
+	cout << "b - a = " << b - a << endl;//运算结果为零,精度丢失
+	return 0;
+}
