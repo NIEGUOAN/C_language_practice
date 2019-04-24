74dd28d601cf5f87bbd03ac5f027c4cdd1dce312
NIEGUOAN <48119707+NIEGUOAN@users.noreply.github.com>
2019-04-24 21:24:17 +0800

    2019/4/24

---
+++ daily/链表.c
@@ -0,0 +1,97 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+typedef struct LNode {
+	int data;
+	struct LNode* next;
+}LNode, * LinkList;
+
+
+
+
+
+int InitList(LinkList* L)
+{
+	*L = (LNode*)malloc(sizeof(LNode));
+	(*L)->next = NULL;
+	return 1;
+}
+
+
+
+int GetElem(LinkList L, int i, int* e)
+{
+	LNode* p = L->next; int j = 1;
+	while (p && j < i)
+	{
+		p = p->next;
+		++j;
+	}
+	if (!p || j > i)
+		return 0;
+	*e = p->data;
+	return 1;
+}
+
+LNode* LocateElem(LinkList L, int e)
+{
+	LNode* p = L->next;
+	while (p && p->data != e)
+		p = p->next;
+	return p;
+}
+
+int ListInsert(LinkList * L, int i, int e)
+{
+	LNode* p = *L; int j = 0;
+	while (p && (j < i - 1))
+	{
+		p = p->next; ++j;
+	}
+	if (!p || j > i - 1)
+		return 0;
+	LNode * s = (LNode*)malloc(sizeof(LNode));
+	s->data = e;
+	s->next = p->next;
+	p->next = s;
+	return 1;
+}
+
+int ListDelete(LinkList * L, int i)
+{
+	LNode* p = (*L); int j = 0;
+	while ((p->next) && (j < i - 1))
+	{
+		p = p->next; ++j;
+	}
+	if (!(p->next) || (j > i - 1))
+		return 0;
+	LNode * q = p->next;
+	p->next = q->next;
+	free(q);
+	return 1;
+}
+
+void CreateList_H(LinkList * L, int n)
+{
+	*L = (LNode*)malloc(sizeof(LNode));
+	(*L)->next = NULL;
+	for (int i = 0; i > n; ++i)
+	{
+		LNode* p = (LNode*)malloc(sizeof(LNode));
+		scanf("%d", &p->data);
+		p->next = (*L)->next; (*L)->next = p;
+	}
+}
+void CreateList_R(LinkList * L, int n)
+{
+	(*L) = (LNode*)malloc(sizeof(LNode));
+	(*L)->next = NULL;
+	LNode* r = (*L);
+	for (int i = 0; i < n; ++i)
+	{
+		LNode* p = (LNode*)malloc(sizeof(LNode));
+		scanf("%d", &p->data);
+		p->next = NULL; r->next = p;
+		r = p;
+	}
+}
---
+++ daily/顺序表.c
@@ -0,0 +1,96 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+typedef struct {
+	int data;
+}Node;
+typedef struct 
+{
+	Node* elem;
+	int length;
+}SqList;
+
+int InitList(SqList* L)
+{
+	L->elem = (Node*)malloc(100 * sizeof(Node));
+	if (L->elem == NULL)
+		return -2;
+	L->length = 0;
+	return 1;
+}
+
+int GetElem(SqList L, int i, Node * e)
+{
+	if (i<1 || i>L.length)
+		return 0;
+	*e = L.elem[i - 1];
+	return 1;
+}
+
+
+
+
+
+
+
+int LocateElem(SqList L, Node e)
+{
+	for (int i = 0; i < L.length; i++)
+		if (L.elem[i].data == e.data)
+			return i + 1;
+	return 0;
+}
+
+int ListInsert(SqList * L, int i, Node e)
+{
+	if ((i < 1) || (i > L->length + 1))
+		return 0;
+	if (L->length == 100)
+		return 0;
+	for (int j = L->length - 1; j >= i - 1; j--)
+		L->elem[j + 1] = L->elem[j];
+	L->elem[i - 1] = e;
+	++L->length;
+	return 1;
+}
+
+
+
+
+int ListDelete(SqList * L, int i)
+{
+	if ((i < 1) || (i > L->length))return 0;
+	for (int j = i; j <= L->length - 1; j++)
+		L->elem[j - 1] = L->elem[j];
+	--L->length;
+	return 1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
